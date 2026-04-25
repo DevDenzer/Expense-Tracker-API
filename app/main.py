0597e7950f899908778abc8c8ce058f325a1b7c6
Zach Denzer <denzerzach5@gmail.com>
2026-04-24 22:15:45 -0500

    fix: update startup logic and database initialization

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from app.routes import expenses
 from app.database import create_table
+from contextlib import asynccontextmanager
 
-app = FastAPI()
-
-@app.on_event("startup")
-def startup():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     create_table()
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(expenses.router)
